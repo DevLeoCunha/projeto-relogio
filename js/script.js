8cc24d2ad3f2bc56b6381d55c75af37c505170a0
Leonardo Cunha <121736215+DevLeoCunha@users.noreply.github.com>
2024-09-01 18:33:54 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,8 +12,19 @@ const getTime = () => {
     const date = new Date();
 
     return {
-        hours: date.getUTCHours(),
+        hours: date.getHours(),
         minutes: date.getMinutes(),
         seconds: date.getSeconds(),
     };  
 }
+
+setInterval(() => { 
+
+    const { seconds, minutes, hours } = getTime();  /* a variavel const para retirar o retorno da função do gettime acima que é um objeto, neste caso retiramos os segundos da função gettime*/ 
+
+    secondHand.style.transform = `translate(0, -50%) rotate(${seconds * 6}deg)`;
+    minuteHand.style.transform = `translate(0, -50%) rotate(${minutes * 6}deg)`;
+    hourHand.style.transform = `translate(0, -50%) rotate(${hours * 30}deg)`;
+
+}, 1000);
+

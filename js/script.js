bf65dcc2e239c73fd3d73cd6c0356ddcdf905882
Leonardo Cunha <121736215+DevLeoCunha@users.noreply.github.com>
2024-09-01 18:08:39 +0100

    js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+/*Sempre por a variável em camelcase, depois seleciona a classe dela do html*/
+
+const hourHand = document.querySelector('.hour-hand');
+const minuteHand = document.querySelector('.minute-hand');
+const secondHand = document.querySelector('.second-hand');
+
+
+
+
+
 const getTime = () => {
     const date = new Date();
+
+    return {
+        hours: date.getUTCHours(),
+        minutes: date.getMinutes(),
+        seconds: date.getSeconds(),
+    };  
 }
